dc904a6efe9d3a32412f072e76ab5517b0b87614
Mykhailo Hrunenko <michagrinenko@gmail.com>
2024-07-22 05:05:01 -0800

    ssadf

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/userRoute.js";
 import waterRouter from "./routes/waterRoute.js";
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerDocument from "./swagger.json" with { type: "json" };
 
 export const app = express();
 
@@ -9,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/users", userRouter);
 app.use("/api/water", waterRouter);
 
